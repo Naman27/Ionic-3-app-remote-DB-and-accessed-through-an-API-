efd584e960aed0071008a298823ef78f726503ea
Naman jain <namanjain.it19@jecrc.ac.in>
2018-01-27 16:46:29 +0530

    Update index.ts

--- sdk/index.ts
+++ sdk/index.ts
@@ -1,37 +1,9 @@
 /**
 * @module SDKModule
-* @author Jonathan Casarrubias <t:@johncasarrubias> <gh:jonathan-casarrubias>
-* @license MIT 2016 Jonathan Casarrubias
+
 * @version 2.1.0
 * @description
-* The SDKModule is a generated Software Development Kit automatically built by
-* the LoopBack SDK Builder open source module.
-*
-* The SDKModule provides Angular 2 >= RC.5 support, which means that NgModules
-* can import this Software Development Kit as follows:
-*
-*
-* APP Route Module Context
-* ============================================================================
-* import { NgModule }       from '@angular/core';
-* import { BrowserModule }  from '@angular/platform-browser';
-* // App Root 
-* import { AppComponent }   from './app.component';
-* // Feature Modules
-* import { SDK[Browser|Node|Native]Module } from './shared/sdk/sdk.module';
-* // Import Routing
-* import { routing }        from './app.routing';
-* @NgModule({
-*  imports: [
-*    BrowserModule,
-*    routing,
-*    SDK[Browser|Node|Native]Module.forRoot()
-*  ],
-*  declarations: [ AppComponent ],
-*  bootstrap:    [ AppComponent ]
-* })
-* export class AppModule { }
-*
+
 **/
 import { JSONSearchParams } from './services/core/search.params';
 import { ErrorHandler } from './services/core/error.service';
